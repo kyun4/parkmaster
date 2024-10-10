e64bd4f5512890b5c024ec83d8a33e95bddeac65
kyun04 <efrencamposagrado0622@gmail.com>
2024-10-10 17:16:50 +0800

    with flexlayout yehey

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,10 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.flexbox.layout)
+
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
